9882692f67d28a1da1dd0c1ae72e77f4f937dcd6
rajib074 <rajib01711173241@gmail.com>
2022-02-16 02:38:28 +0600

    all

--- shopping-cart-main/app.js
+++ shopping-cart-main/app.js
@@ -1,5 +1,5 @@
-function updateCaseNumber(isIncreasing){
-    const caseInput = document.getElementById('case-number')
+function updateCaseNumber(product,price,isIncreasing){
+    const caseInput = document.getElementById(product+ '-number')
     let caseNumber = caseInput.value;
     if(isIncreasing){
         caseNumber = parseInt(caseNumber) + 1;
@@ -9,22 +9,22 @@ function updateCaseNumber(isIncreasing){
     }  
     caseInput.value = caseNumber;  
     // update Case Total 
-    const caseTotal = document.getElementById('case-total')
-    caseTotal.innerText = caseNumber * 59; 
+    const caseTotal = document.getElementById(product+'-total')
+    caseTotal.innerText = caseNumber * price; 
 }
 // phone increase decrase events 
 document.getElementById('phone-plus').addEventListener('click', function(){
-    
+    updateCaseNumber('phone', 1219, true);
 })
 
 
 // handle case increase decrase events 
 document.getElementById('case-pius').addEventListener('click',function(){ 
-    updateCaseNumber(true);
+    updateCaseNumber('case', 59, true);
     
    
 })
 document.getElementById('case-minus').addEventListener('click',function(){ 
-    updateCaseNumber(false);    
+    updateCaseNumber('case', 59, false);    
       
 })
